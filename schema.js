e7c399df437d1dc607daeebd1ec799b9619349f7
Julie Assur <78449568+limzkil@users.noreply.github.com>
2022-03-23 17:28:48 -0400

    Update and rename schema.js to Message.js

--- schema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/messages');
-const db = mongoose.connection
-
-db.on('error', console.error.bind(console, 'connection error'))
-
-const messageSchema = new mongoose.Schema({
-})
